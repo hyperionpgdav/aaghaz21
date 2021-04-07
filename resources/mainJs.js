b2b32ab1021fd1f9e5e91efe9f4fb2f20ca93380
hyperionpgdav <82092767+hyperionpgdav@users.noreply.github.com>
2021-04-07 17:21:41 +0530

    Add files via upload

---
+++ resources/mainJs.js
@@ -0,0 +1,7 @@
+/* Function for opening and closing of Side Menu Bar*/
+function openMenu(){
+    document.getElementById("sideMenu").style.width = "80%";
+}
+function closeMenu(){
+    document.getElementById("sideMenu").style.width = "0";
+}
