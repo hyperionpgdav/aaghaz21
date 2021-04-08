122f39ae0a4dca2d7f9ceff6de6b11dda8fe66d5
hyperionpgdav <82092767+hyperionpgdav@users.noreply.github.com>
2021-04-08 21:55:03 +0530

    Delete mainJs.js

--- resources/mainJs.js
+++
@@ -1,7 +0,0 @@
-/* Function for opening and closing of Side Menu Bar*/
-function openMenu(){
-    document.getElementById("sideMenu").style.width = "80%";
-}
-function closeMenu(){
-    document.getElementById("sideMenu").style.width = "0";
-}
